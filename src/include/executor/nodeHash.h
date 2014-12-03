4300b15a6cb257d195e94574fc07decca666f152
embarg0 <emilienne.pugin@gmail.com>
2014-12-02 22:56:57 -0500

    Revert "added nodehash.h change for previous commit"
    This reverts commit 09ba568cbe0f9ffdd478f274948c28ab8788fe88.

--- src/include/executor/nodeHash.h
+++ src/include/executor/nodeHash.h
@@ -35,7 +35,9 @@ extern void ExecHashGetBucketAndBatch(HashJoinTable hashtable,
 						  uint32 hashvalue,
 						  int *bucketno,
 						  int *batchno);
-extern HeapTuple ExecScanHashBucket(HashJoinState *hjstate,
+extern HeapTuple ExecScanHashBucket_probingOuter(HashJoinState *hjstate,
+                                    ExprContext *econtext);
+extern HeapTuple ExecScanHashBucket_probingInner(HashJoinState *hjstate,
                                     ExprContext *econtext);
 extern void ExecHashTableReset(HashJoinTable hashtable);
 extern void ExecChooseHashTableSize(double ntuples, int tupwidth,
